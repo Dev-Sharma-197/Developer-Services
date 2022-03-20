29c45952179ed7b7b1a603d0de167bce4d23e343
Dev Sharma <ginnisharma1972000@gmail.com>
2022-03-20 14:17:05 +0530

    updating package file

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,7 +1,4 @@
 const mongoose = require("mongoose");
-// const server = "localhost:27017";
-const password = "Dev@9728942296";
-const database = "devtest";
 const uri = `mongodb+srv://Dev_sharma_19:Dev%409728942296@mycluster-dev.fpezx.mongodb.net/devtest?retryWrites=true&w=majority`;
 
 mongoose.connect(uri, {
